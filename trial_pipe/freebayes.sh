045912c1e4852af1fd6b69e6a6c239806abdff9e
WLF1115 <wanglinfeng1115@gmail.com>
2021-12-17 00:01:30 +0000

    created strain_analysis script

--- trial_pipe/freebayes.sh
+++ trial_pipe/freebayes.sh
@@ -1,28 +1,29 @@
 #freebayes bam to vcf conversion
+#local path: cd ~/trial_tb_philippines/trial pipelines/Genomic_data_analysis/trial_pipe
 
-BASE='data/trial'
-RAW='data/trial/raw_fastq'
-PROCESSED='data/trial/processed'
-#reference file
-REFGENOME='refgenome/MTB-h37rv_asm19595v2-eg18.fa'
+RAW='../../../data/raw_fastq'
+PROCESSED='../../../data/processed'
+REFGENOME='../../../refgenome/MTB-h37rv_asm19595v2-eg18.fa'
+PIPELINE='~/trial_tb_philippines/trial pipelines/Genomic_data_analysis/trial_pipe'
+
 
 #activating variant_detection enviroment
 echo 'activating variant_detection enviroment'
 eval "$(conda shell.bash hook)"
 conda activate variant_detection 
 
-mkdir ~/$PROCESSED/freebayesVCF
-cd ~/$PROCESSED/freebayesVCF
+mkdir $PROCESSED/freebayesVCF
+cd $PROCESSED/freebayesVCF
 
 #Converting bam to vcf file
 echo 'converting .bam to .vcf for sample' $1 
-freebayes -f ~/$REFGENOME ~/$PROCESSED/pool_f4m/$1.mkdup.bam > ~/$PROCESSED/freebayesVCF/$1.vcf
+freebayes -f $REFGENOME $PROCESSED/pool_f4m/$1.mkdup.bam > $PROCESSED/freebayesVCF/$1.vcf
 
 
-#freebayes -f ~/$REFGENOME ~/$PROCESSED/pool_f4m/ERR6634978.mkdup.bam > ~/$PROCESSED/freebayesVCF/ERR6634978.vcf
+#freebayes -f $REFGENOME $PROCESSED/pool_f4m/ERR6634978.mkdup.bam > $PROCESSED/freebayesVCF/ERR6634978.vcf
 
 #Quality filtering
 echo 'Quality filtering' $1 
-vcftools --vcf ~/$PROCESSED/freebayesVCF/$1.vcf --minQ 20 --recode --recode-INFO-all --out $1_q20
+vcftools --vcf $PROCESSED/freebayesVCF/$1.vcf --minQ 20 --recode --recode-INFO-all --out $1_q20
 
 
--- trial_pipe/gwas.sh
+++ trial_pipe/gwas.sh
@@ -1,17 +1,22 @@
+#local path: cd ~/trial_tb_philippines/trial pipelines/Genomic_data_analysis/trial_pipe
+
+
 #BASE='data/trial'
-RAW='data/trial/raw_fastq'
-PROCESSED='data/trial/processed'
-PIPELINE='pipelines/Genomic_data_analysis/trial_pipe'
+RAW='../../../data/raw_fastq'
+PROCESSED='../../../data/processed'
+REFGENOME='../../../refgenome/MTB-h37rv_asm19595v2-eg18.fa'
+PIPELINE='~/trial_tb_philippines/trial pipelines/Genomic_data_analysis/trial_pipe'
+
 
 #activating environment
 eval "$(conda shell.bash hook)"
 conda activate gwas
 
 
-mkdir ~/$PROCESSED/plink
-cd ~/$PROCESSED/plink
+mkdir $PROCESSED/plink
+cd $PROCESSED/plink
 
 #generates plink file
 echo 'generating plink file'
-# bgzip ~/$PROCESSED/freebayesVCF/$1_q20.recode.vcf
-plink --vcf ~/$PROCESSED/freebayesVCF/$1_q20.recode.vcf --make-bed --out $1 --allow-extra-chr
+# bgzip $PROCESSED/freebayesVCF/$1_q20.recode.vcf
+plink --vcf $PROCESSED/freebayesVCF/$1_q20.recode.vcf --make-bed --out $1 --allow-extra-chr
