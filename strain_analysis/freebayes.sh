cd61fa7c0b76dfe37abd52a454f581f73f5cd87a
WLF1115 <wanglinfeng1115@gmail.com>
2022-01-24 16:13:47 +0000

    try to improve model with new exclusions and depth filter

--- strain_analysis/freebayes.sh
+++ strain_analysis/freebayes.sh
@@ -11,18 +11,19 @@ echo 'activating variant_detection enviroment'
 eval "$(conda shell.bash hook)"
 conda activate variant_detection 
 
-mkdir -p $PROCESSED/freebayesVCF
-cd $PROCESSED/freebayesVCF
+mkdir -p $PROCESSED/freebayesVCF/q20
+cd $PROCESSED/freebayesVCF/q20
 
 #Converting bam to vcf file
 echo 'converting .bam to .vcf for sample' $1 
-freebayes -f $REFGENOME $PROCESSED/$1.mkdup.bam > $PROCESSED/freebayesVCF/$1.vcf
-bgzip $1.vcf
+freebayes -f $REFGENOME $PROCESSED/$1.mkdup.bam > $PROCESSED/freebayesVCF/q20/$1.vcf
 
 #freebayes -f $REFGENOME $PROCESSED/pool_f4m/ERR6634978.mkdup.bam > $PROCESSED/freebayesVCF/ERR6634978.vcf
 
 #Quality filtering
-# echo 'Quality filtering' $1 
-# vcftools --vcf $PROCESSED/freebayesVCF/$1.vcf --minQ 20 --recode --recode-INFO-all --out $1
+echo 'Quality filtering' $1 
+vcftools --vcf $PROCESSED/freebayesVCF/q20/$1.vcf --minQ 20 --recode --recode-INFO-all --out $1_q20
+
+bgzip $1_q20.vcf
 
 
