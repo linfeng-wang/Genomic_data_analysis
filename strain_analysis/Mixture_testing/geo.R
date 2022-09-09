b50f0f94ff3cd94686ce67d9b332458a32a2df29
WLF1115 <wanglinfeng1115@gmail.com>
2022-09-09 17:07:18 +0000

    finally pie on map

--- strain_analysis/Mixture_testing/geo.R
+++ strain_analysis/Mixture_testing/geo.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(httpgd)
 library(tmap)    # for static and interactive maps
 library(jsonlite)
 library(leaflet) # for interactive maps
@@ -6,3 +7,15 @@ library(leaflet) # for interactive maps
 cars <- c(1, 3, 6, 4, 9)
 plot(cars)
 
+
+print(cars)
+
+ggplot(cars, aes(x=cars, y=cars))+geom_point()
+
+data <- data.frame(x=rnorm(30))
+ 
+# Basic plot with title
+ggplot( data=data, aes(x=x)) + 
+  geom_histogram(fill="skyblue", alpha=0.5, bins=10) +
+  ggtitle("A blue Histogram") +
+  theme_minimal()
